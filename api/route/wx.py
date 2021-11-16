40ea0da5a70d2382850f46162e3fcbfd29afd783
zhulei07 <zhulei07@meituan.com>
2021-11-16 23:13:40 +0800

    init

---
+++ api/route/wx.py
@@ -0,0 +1,34 @@
+'''
+Description: wx
+Author: Monkee Lei
+Date: 2021/11/15
+'''
+from http import HTTPStatus
+from flask import Blueprint, request
+from flasgger import swag_from
+from api.model.welcome import WelcomeModel
+from api.schema.welcome import WelcomeSchema
+
+wx_api = Blueprint('wx', __name__)
+
+
+@wx_api.route('/')
+@swag_from({
+    'responses': {
+        HTTPStatus.OK.value: {
+            'description': 'Welcome to the Flask Starter Kit',
+            'schema': WelcomeSchema
+        }
+    }
+})
+def wx():
+    """
+    1 liner about the route
+    A more detailed description of the endpoint
+    ---
+    """
+    signature = request.args.get('signature', default = '', type = str)
+    echostr = request.args.get('echostr', default = '', type = str)
+    timestamp = request.args.get('timestamp', default = '', type = str)
+    nonce = request.args.get('nonce', default = '', type = str)
+    return echostr, 200
--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+'''
+Description: 
+Author: Monkee Lei
+Date: 2021/11/15
+'''
 from flask import Flask
 from flasgger import Swagger
 from api.route.home import home_api
+from api.route.wx import wx_api
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +18,7 @@ def create_app():
      ## Initialize Config
     app.config.from_pyfile('config.py')
     app.register_blueprint(home_api, url_prefix='/api')
+    app.register_blueprint(wx_api, url_prefix='/wx')
 
     return app
 
